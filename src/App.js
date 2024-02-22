0ea268bc55fef8a7523b1657c25f31f5b2363c22
Q3C-rypt <jacobantonious@rogers.com>
2024-02-22 01:04:35 -0500

    gh-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import dataBase from './firebase';
+import firebase from 'firebase/compat/app';
+import video from './assets/binary_back.mp4';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+        const [input, setInput] = useState("");
+        const [message, setMessage] = useState("");
+
+        const inputHandler = (e) => {
+                setInput(e.target.value);
+        }
+        const submitHandler = (e) => {
+                e.preventDefault();
+                if (input){
+                    console.log(input);
+
+                    dataBase.collection("emails").add({
+                        email: input,
+                        time: firebase.firestore.FieldValue.serverTimestamp(),
+                    });
+
+                    setInput("");
+                    setMessage("Email logged! You will hear news from us shortly 🙂")
+                    setTimeout (() => {
+                        setMessage(""); 
+                    }, 10000);
+                }
+        }
+       
+    return (
+        <div>
+            <div className="sign-up">
+                <video src={video} autoPlay muted loop playbackRate={0.5} />
+                <h2 className = "text" >Welcome To Q3C!</h2>
+                <h1 className = "des">Queen's Cryptography and Cybersecurity Club!</h1>
+                <h6 className = "des_det">
+                    Sign up with your email below to stay up to date with all future club activities.
+                    This includes projects, workshops and general member applications!
+                </h6>
+                <form onSubmit={submitHandler} className = "blur_box">
+                    <h2 className = "Sub" >Please provide your Email: </h2>
+                    <input type="email" className = "email" onChange={inputHandler} value = {input}/>
+                    <button type="submit" className="submit-button">
+                        <text className= "submit" >Submit</text>
+                    </button>
+                    {message && <alert className = "alert">
+                    {message}
+                    </alert>}
+                </form>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
